3afa1b3c6b079d978657d157328b4537c151dd81
zhouhuafei <zhouhuafei@shopex.cn>
2018-05-18 18:52:39 +0800

    md

--- readme.md
+++ readme.md
@@ -2,6 +2,25 @@
 * 0依赖，支持，amd，commonjs，browser
 ```
 const keepDecimal = require('zhf.keep-decimal');
-keepDecimal(10, 3); // '10.000'
+
+keepDecimal(); // '0.00'
+keepDecimal(''); // ''
+keepDecimal(0); // '0.00'
+keepDecimal('0'); // '0.00'
+keepDecimal('哈哈'); // ''
+keepDecimal('1哈哈1'); // '1.10'
+keepDecimal('哈哈2'); // '2.00'
+keepDecimal('-哈哈2'); // '-2.00'
+keepDecimal('-.哈.00.哈2'); // '-0.20'
+keepDecimal('', 0); // ''
+keepDecimal('哈哈', 0); // ''
+keepDecimal('1哈哈1', 0); // '1'
+keepDecimal('哈哈2', 0); // '2'
+keepDecimal('-哈哈2', 0); // '-2'
+keepDecimal('-.哈.00.哈2', 0); // '0'
 keepDecimal(8, 2); // '8.00'
+keepDecimal(10, 3); // '10.000'
+keepDecimal('-0001.01', 3); // '-1.010'
+keepDecimal('0001.01', 4); // '1.0100'
+keepDecimal('10.2', 2); // '10.20'
 ```
--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,8 @@ const keepDecimal = require('../dist/index.min');
 test(`保留几位小数`, () => {
     expect(keepDecimal()).toEqual('0.00');
     expect(keepDecimal('')).toEqual('');
+    expect(keepDecimal(0)).toEqual('0.00');
+    expect(keepDecimal('0')).toEqual('0.00');
     expect(keepDecimal('哈哈')).toEqual('');
     expect(keepDecimal('1哈哈1')).toEqual('1.10');
     expect(keepDecimal('哈哈2')).toEqual('2.00');
@@ -16,4 +18,7 @@ test(`保留几位小数`, () => {
     expect(keepDecimal('-.哈.00.哈2', 0)).toEqual('0');
     expect(keepDecimal(8, 2)).toEqual('8.00');
     expect(keepDecimal(10, 3)).toEqual('10.000');
+    expect(keepDecimal('-0001.01', 3)).toEqual('-1.010');
+    expect(keepDecimal('0001.01', 4)).toEqual('1.0100');
+    expect(keepDecimal('10.2', 2)).toEqual('10.20');
 });
